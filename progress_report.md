694724fceca4559351a2d36671018325a96d8137
Emma McKibbin <ecm68@pitt.edu>
2022-02-27 03:39:47 -0500

    added progress report 1 files

--- progress_report.md
+++ progress_report.md
@@ -8,3 +8,16 @@ Emma McKibbin | LING 1340
 - README drafted
 - private/ and data/ folders created on local machine (ignored by git)
 - **Project Plan committed**: based on previous project ideas submission
+
+## 1st Progress Report:
+### Data Collection
+
+#### **2/23/22:**
+- **Learning BeautifulSoup**: using `beautifulsoup4` and `requests` packages to retrieve and parse HTML code.
+- **Retrieved musician lists**: as a practice exercise, parsing the Wiki (111 artists). (wiki_musicians.py)
+
+#### **2/26/22:**
+- **Got lyrics from Genius**: using John W. Miller's `lyricsgenius` package and the Genius API, I scraped lyrics from 70+ outsider musicians. (lyricsgenius_requests.ipynb)
+- **Created data_samples directory**: includes the Crispin Glover JSON, used as an example in lyricsgenius_requests.ipynb.
+- **Created a lyrics data frame**: this contains 3,338 lyrics from 63 artists and is not yet cleaned.  (load_json_to_df.ipynb)
+- **Began data exploration and cleaning**: (data_cleaning_and_exploration.ipynb)
---
+++ wiki_musicians.py
@@ -0,0 +1,36 @@
+# This script retrieves the 111 musician names from the Wiki page
+# for outsider musicians and saves the list to a .csv.
+# This is VERY likely to become DEPRECATED.  There is a hard-coded
+# number of alphabetical categories (<26) that this script expects to 
+# be in the Wikipedia list, which is specific to this point in time
+# (Feb 2022).
+
+# imports
+print("Importing bs4...")
+from bs4 import BeautifulSoup
+print("Importing requests...")
+import requests
+
+# get Wiki HTML
+url = "https://en.wikipedia.org/wiki/Category:Outsider_musicians"
+print("Getting results...")
+result = requests.get(url)
+print("Completed!\n")
+
+# parse Wiki HTML with bs4
+outsiderwiki = BeautifulSoup(result.text, "html.parser")
+
+# find all 111 musician names from the list
+musicians = []
+# this is tailored to the list at this specific point in time
+for item in outsiderwiki.find_all("ul")[1:23]:
+    for string in item.strings:
+        if string != "\n":
+            musicians.append(repr(string))
+
+# save uncleaned list to file
+file = open("wikimusicians.txt", "w")
+for m in musicians:
+    file.write(m.strip("''")+"\n")
+file.close()
+
